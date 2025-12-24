3c63e9d162a7241d18789214703663a5499ce990
Nika Kurafeeva <nikurafeeva@gmail.com>
2025-12-24 15:52:42 +0300

    Add login page fixture and tests

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,5 +1,7 @@
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
 
 
 class LoginPage(BasePage):
@@ -12,7 +14,22 @@ class LoginPage(BasePage):
     LOGIN_ERROR_LOCATOR = (By.CSS_SELECTOR, "#login_button_container > div > form > div.error-message-container.error > h3")
     LOCKED_OUT_USER_ERROR_LOCATOR =(By.CSS_SELECTOR, "#login_button_container > div > form > div.error-message-container.error")
 
+    COMMON_PASSWORD = "secret_sauce"
+    STANDARD_USERNAME = "standard_user"
+    LOCKED_OUT_USERNAME = "locked_out_user"
+    PERFORMANCE_GLITCH_USERNAME = "performance_glitch_user"
+
+    @property
+    def current_url(self) -> str:
+        return self.driver.current_url
+
+    def wait_for_elements(self) -> None:
+        self.wait.until(EC.visibility_of_element_located(self.USERNAME_LOCATOR))
+        self.wait.until(EC.visibility_of_element_located(self.PASSWORD_LOCATOR))
+        self.wait.until(EC.visibility_of_element_located(self.LOGIN_BUTTON_LOCATOR))
+
     def login(self, username:str, password:str) -> None:
         self.driver.find_element(*self.USERNAME_LOCATOR).send_keys(username)
         self.driver.find_element(*self.PASSWORD_LOCATOR).send_keys(password)
         self.driver.find_element(*self.LOGIN_BUTTON_LOCATOR).click()
+
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,7 +1,21 @@
+from selenium.common import TimeoutException
 from pages.login_page import LoginPage
+import pytest
 
 
-def test_login_page_opens(driver):
-    site_obj = LoginPage(driver)
-    site_obj.open()
-    assert site_obj.driver.current_url == site_obj.page_url
+# to check if page loaded
+@pytest.fixture
+def login_page(driver) -> LoginPage:
+    page = LoginPage(driver)
+    try:
+        page.open()
+        page.wait_for_url(page.page_url)
+        page.wait_for_elements()
+    except TimeoutException:
+        pytest.skip("Login page or elements did not load")
+    return page
+
+def test_login(login_page):
+    login_page.login(login_page.STANDARD_USERNAME, login_page.COMMON_PASSWORD)
+    login_page.wait_for_url(login_page.INVENTORY_URL)
+    assert login_page.current_url == login_page.INVENTORY_URL
