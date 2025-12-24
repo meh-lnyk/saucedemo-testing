17e72a2b7f6f8f6759e3ac61da873ca1ac2d6283
Nika Kurafeeva <nikurafeeva@gmail.com>
2025-12-24 18:00:52 +0300

    Add test for locked out user

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,7 +3,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 
-
 class LoginPage(BasePage):
     page_url = "https://www.saucedemo.com/"
     INVENTORY_URL = "https://www.saucedemo.com/inventory.html"
@@ -27,6 +26,9 @@ class LoginPage(BasePage):
     def has_login_error_message(self) -> bool:
         return self.driver.find_element(*self.LOGIN_ERROR_LOCATOR)
 
+    def has_locked_out_message(self) -> bool:
+        return self.driver.find_element(*self.LOCKED_OUT_USER_ERROR_LOCATOR)
+
     def wait_for_elements(self) -> None:
         self.wait.until(EC.visibility_of_element_located(self.USERNAME_LOCATOR))
         self.wait.until(EC.visibility_of_element_located(self.PASSWORD_LOCATOR))
@@ -36,4 +38,3 @@ class LoginPage(BasePage):
         self.driver.find_element(*self.USERNAME_LOCATOR).send_keys(username)
         self.driver.find_element(*self.PASSWORD_LOCATOR).send_keys(password)
         self.driver.find_element(*self.LOGIN_BUTTON_LOCATOR).click()
-
